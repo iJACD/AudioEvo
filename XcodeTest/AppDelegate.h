ec04fe55163d713ed92fa401b1eb007f31059aaf
JohnAnthony <iBlasian@johnanthonys-macbook-pro.local>
2016-08-21 00:32:55 -0400

    Initial Commit

---
+++ XcodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeTest
+//
+//  Created by JohnAnthony on 8/21/16.
+//  Copyright © 2016 JohnAnthony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
